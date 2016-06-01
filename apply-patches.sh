fb8a59f1fcda2d0ca4e109e0e1c180ff3c2f61fb
DarkKnight6499 <yazad.madan@gmail.com>
2016-06-01 19:57:35 +0530

    Add MTK patches

--- apply-patches.sh
+++ apply-patches.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 cd ../../../..
 # Patches made by Rohan Taneja
-# only for personal use and TRT
 cd external/sepolicy
-git apply -v ../../device/mediatek/mt6582/patches/external_sepolicy.patch
+git apply -v ../../device/Huawei/Holly/patches/external_sepolicy.patch
 cd ../..
 cd frameworks/native
-git apply -v ../../device/mediatek/mt6582/patches/framework_native.patch
+git apply -v ../../device/Huawei/Holly/patches/framework_native.patch
 cd ../..
 cd frameworks/av
-git apply -v ../../device/mediatek/mt6582/patches/frameworks_av.patch
+git apply -v ../../device/Huawei/Holly/patches/frameworks_av.patch
 cd ../..
 cd frameworks/base
-git apply -v ../../device/mediatek/mt6582/patches/frameworks_base.patch
+git apply -v ../../device/Huawei/Holly/patches/frameworks_base.patch
 cd ../..
 cd frameworks/opt/telephony
-git apply -v ../../../device/mediatek/mt6582/patches/frameworks_opt_telephony.patch
+git apply -v ../../../device/Huawei/Holly/patches/frameworks_opt_telephony.patch
 cd ../../..
 cd hardware/libhardware
-git apply -v ../../device/mediatek/mt6582/patches/hardware_libhardware.patch
+git apply -v ../../device/Huawei/Holly/patches/hardware_libhardware.patch
 cd ../..
 cd system/bt
-git apply -v ../../device/mediatek/mt6582/patches/system_bt.patch
+git apply -v ../../device/Huawei/Holly/patches/system_bt.patch
 cd ../..
 cd system/core
-git apply -v ../../device/mediatek/mt6582/patches/system_core.patch
+git apply -v ../../device/Huawei/Holly/patches/system_core.patch
 cd ../..
 cd system/netd
-git apply -v ../../device/mediatek/mt6582/patches/system_netd.patch
+git apply -v ../../device/Huawei/Holly/patches/system_netd.patch
 cd ../..
 echo Patches Applied Successfully!
 echo Made by Rohan Taneja...
